3ce186cff8646688da551474bd32e1c4f7d26cd2
fareshossam2 <fareshossam201@gmail.com>
2024-02-15 01:09:33 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+    
+    $("#bar").click(function() {
+       
+        $("nav ul").css("right", " -100px");
+       
+    });
+    $("#close").click(function() {
+       
+        $("nav ul").css({
+            "right": "-3000px",
+            "transition": "right 0.7s ease-in-out" 
+          });
+    });
+    $("li").click(function() {
+       
+        $("nav ul").css("right", " -3000px");
+
+    });
+    $("#sign").click(function() {
+       
+        $("nav ul").css("right", " -3000px");
+
+    });
+
+    });
+
+    function clear(){
+        document.getElementById('myForm').reset(); 
+    
+    }
+
+    $(document).ready(function () {
+        $("#education").hide();
+        $("#skills").hide();
+        $("#links").hide();
+        $("#personal").show();
+        $(".personal").css("color" , "royalblue");
+
+        $(".personal").click(function() {
+           
+            $("#education").hide();
+            $("#links").hide();
+            $("#personal").slideDown(3000);   
+            $(".personal").css("color" , "royalblue");
+            $(".education").css("color" , "aliceblue");
+            $(".links").css("color" , "aliceblue");
+            $(".skills").css("color" , "aliceblue");
+
+
+                  
+        });
+        $(".education").click(function() {
+           
+            $("#personal").hide();
+            $("#links").hide();
+            $("#skills").hide();
+            $("#education").slideDown(3000);   
+            $(".personal").css("color" , "aliceblue");
+            $(".education").css("color" , "royalblue");
+            $(".links").css("color" , "aliceblue");
+            $(".skills").css("color" , "aliceblue");
+ 
+        });
+        $(".links").click(function() {
+           
+            $("#personal").hide();
+            $("#education").hide();
+            $("#skills").hide();
+            $("#links").slideDown(3000);  
+            $(".personal").css("color" , "aliceblue");
+            $(".education").css("color" , "aliceblue");
+            $(".links").css("color" , "royalblue"); 
+            $(".skills").css("color" , "aliceblue");
+   
+        });
+
+        $(".skills").click(function() {
+
+        $("#education").hide();
+        $("#skills").slideDown(3000)
+        $("#links").hide();
+        $("#personal").hide();
+        $(".skills").css("color" , "royalblue");
+        $(".personal").css("color" , "aliceblue");
+        $(".education").css("color" , "aliceblue");
+        $(".links").css("color" , "aliceblue");
+    });
+
+    });
